186201975c83ce8da3b24a198aabe2629157ba4e
Deniz Cakmak <d.cakmak@emakina.com.tr>
2023-05-02 11:06:44 +0300

    Fix utilities IndexOf and Sort methods based on the tests.

--- Task1/Product.cs
+++ Task1/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     public class Product
     {
@@ -11,5 +13,31 @@
         public string Name { get; set; }
 
         public double Price { get; set; }
+
+        /// <summary>
+        /// Check if the obj is equals the current instance of Product.
+        /// </summary>
+        /// <param name="obj">Product object.</param>
+        /// <returns> Return true if the parameter equals the current instance of the Product, else return false. </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Product product = obj as Product;
+            return Name == product.Name && Price == product.Price;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Price);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
--- Task1/Utilities.cs
+++ Task1/Utilities.cs
@@ -8,39 +8,56 @@ namespace Task1
         /// Sorts an array in ascending order using bubble sort.
         /// </summary>
         /// <param name="numbers">Numbers to sort.</param>
+        /// <exception cref="ArgumentNullException">Throws ArgumentNullException if the numbers array is null.</exception>
         public static void Sort(int[] numbers)
         {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException(nameof(numbers));
+            }
+
             int temp;
             for (int i = 0; i < numbers.Length; i++)
             {
-                for (int j = i; j < numbers.Length; j++)
+                for (int j = i + 1; j < numbers.Length; j++)
                 {
-                    if (numbers[i] < numbers[j])
+                    if (numbers[i] > numbers[j])
                     {
                         temp = numbers[i];
-                        numbers[i] = temp;
-                        numbers[j] = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
                     }
                 }
             }
         }
 
         /// <summary>
-        /// Searches for the index of a product in an <paramref name="products"/> 
+        /// Searches for the index of a product in an <paramref name="products"/>
         /// based on a <paramref name="predicate"/>.
         /// </summary>
         /// <param name="products">Products used for searching.</param>
         /// <param name="predicate">Product predicate.</param>
         /// <returns>If match found then returns index of product in <paramref name="products"/>
         /// otherwise -1.</returns>
+        /// <exception cref="ArgumentNullException">Throws ArgumentNullException if either products array or predicate is null.</exception>
         public static int IndexOf(Product[] products, Predicate<Product> predicate)
         {
-            for (int i = 0; i < products.Length - 1; i++)
+            if (products == null)
             {
-                var product = products[i - 1];
+                throw new ArgumentNullException(nameof(products));
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                var product = products[i];
                 if (predicate(product))
                 {
-                    return --i;
+                    return i;
                 }
             }
 
